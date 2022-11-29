a3854765442500b66989bb74e8706da5cc46f8f7
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-11-29 11:37:01 -0500

    updated User model

--- server/models/User.js
+++ server/models/User.js
@@ -21,6 +21,11 @@ const UserSchema = new mongoose.Schema(
             max: 50,
             unique: true,
         },
+        password: {
+          type: String,
+          required: true,
+          min: 5,  
+        },
         picturePath: {
             type:String,
             default: []

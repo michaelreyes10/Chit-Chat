478a3ac8f1f2d87fcf1964f53daa0ee1b3d6194f
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-11-29 16:48:13 -0500

    added login

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -38,6 +39,8 @@ const storage = multer.diskStorage({
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 
+/* ROUTES */
+app.use("./auth", authRoutes)
   /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose

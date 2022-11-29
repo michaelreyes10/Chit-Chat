a66893777bd461c43a7ee93c15b44051b4cf3b2b
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-11-29 11:34:38 -0500

    added User model

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -31,8 +32,11 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
       cb(null, file.originalname);
     },
-  });
+});
   const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
   /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
